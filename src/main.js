4bc1cdddf084ad3830d8f30702d51cc225cee9d4
Utkarsh Tiwari <utkarsh.nits@gmail.com>
2023-01-27 15:46:07 +0530

    code refactoring

--- src/main.js
+++ src/main.js
@@ -1,19 +1,18 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 import axios from 'axios';
-import '../node_modules/bulma/css/bulma.css'
+import '../node_modules/bulma/css/bulma.css';
 import '@/assets/styles.css';
 
 const baseUrl = process.env.NODE_ENV === 'development'
               ? 'http://localhost:4444/' : 'https://foody-duniya-api.herokuapp.com/';
 axios.defaults.baseURL = baseUrl;
-// axios.defaults.baseURL = 'https://foody-duniya-api.herokuapp.com/';
 axios.interceptors.request.use(config => {
   const token = localStorage.getItem("token");
   config.headers["Authorization"] = `Bearer ${token}`;
   return config;
 });
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).mount('#app');
