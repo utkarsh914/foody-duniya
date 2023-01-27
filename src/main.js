20f4af64fbf2d98e796a62d94ea5c83d3c8ce97e
Utkarsh Tiwari <utkarsh.nits@gmail.com>
2023-01-27 15:42:45 +0530

    set bas eurl based on dev/prod

--- src/main.js
+++ src/main.js
@@ -6,8 +6,9 @@ import axios from 'axios';
 import '../node_modules/bulma/css/bulma.css'
 import '@/assets/styles.css';
 
-// axios.defaults.withCredentials = true;
-axios.defaults.baseURL = 'http://localhost:4444/';
+const baseUrl = process.env.NODE_ENV === 'development'
+              ? 'http://localhost:4444/' : 'https://foody-duniya-api.herokuapp.com/';
+axios.defaults.baseURL = baseUrl;
 // axios.defaults.baseURL = 'https://foody-duniya-api.herokuapp.com/';
 axios.interceptors.request.use(config => {
   const token = localStorage.getItem("token");
